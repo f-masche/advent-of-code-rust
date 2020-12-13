198cef49cb8a32785b682fa1be1dd8618cb539e4
Fabian Maschewski <fabian.maschewski@kaiser-x.com>
2020-12-13 10:04:21 +0100

    add utils

--- src/day_13.rs
+++ src/day_13.rs
@@ -1,4 +1,5 @@
 use crate::puzzle::Puzzle;
+use crate::utils::chinese_remainder;
 
 pub fn run() {
   let a = Puzzle {
@@ -62,35 +63,4 @@ fn solution_b(input: &String) -> Option<String> {
 
   let result = chinese_remainder(&residues, &modulii);
   result.map(|x| x.to_string())
-}
-
-fn egcd(a: i64, b: i64) -> (i64, i64, i64) {
-  if a == 0 {
-    (b, 0, 1)
-  } else {
-    let (g, x, y) = egcd(b % a, a);
-    (g, y - (b / a) * x, x)
-  }
-}
-
-fn mod_inv(x: i64, n: i64) -> Option<i64> {
-  let (g, x, _) = egcd(x, n);
-  if g == 1 {
-    Some((x % n + n) % n)
-  } else {
-    None
-  }
-}
-
-fn chinese_remainder(residues: &Vec<i64>, modulii: &Vec<i64>) -> Option<i64> {
-  let prod = modulii.iter().filter(|x| **x > 0).product::<i64>();
-
-  let mut sum = 0;
-
-  for (&residue, &modulus) in residues.iter().zip(modulii) {
-    let p = prod / modulus;
-    sum += residue * mod_inv(p, modulus)? * p
-  }
-
-  Some(sum % prod)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,34 @@
 #[macro_use]
 extern crate lazy_static;
 
-// mod day_01;
-// mod day_02;
-// mod day_03;
-// mod day_04;
-// mod day_05;
-// mod day_06;
-// mod day_07;
-// mod day_08;
-// mod day_09;
-// mod day_10;
-// mod day_11;
-// mod day_12;
+mod day_01;
+mod day_02;
+mod day_03;
+mod day_04;
+mod day_05;
+mod day_06;
+mod day_07;
+mod day_08;
+mod day_09;
+mod day_10;
+mod day_11;
+mod day_12;
 mod day_13;
 mod puzzle;
+mod utils;
 
 fn main() {
-  // day_01::run();
-  // day_02::run();
-  // day_03::run();
-  // day_04::run();
-  // day_05::run();
-  // day_06::run();
-  // day_07::run();
-  // day_08::run();
-  // day_09::run();
-  // day_10::run();
-  // day_11::run();
-  // day_12::run();
+  day_01::run();
+  day_02::run();
+  day_03::run();
+  day_04::run();
+  day_05::run();
+  day_06::run();
+  day_07::run();
+  day_08::run();
+  day_09::run();
+  day_10::run();
+  day_11::run();
+  day_12::run();
   day_13::run();
 }
